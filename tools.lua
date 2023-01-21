920f4ff0506045960f4e33f989241c0f677d104b
szze <106690765+CF-Trail@users.noreply.github.com>
2023-01-21 22:56:05 +0300

    Update tools.lua

--- tools.lua
+++ tools.lua
@@ -14,7 +14,7 @@ function cmds:GetCharacter(plr)
    return players:FindFirstChild(plr).Character or players:FindFirstChild(plr).CharacterAdded:Wait()
 end
 
-function cmds:LocalPrimaryPart()
+function cmds:GetLocalRoot()
    return (LP.Character or LP.CharacterAdded:Wait()):FindFirstChildOfClass('Humanoid').RootPart
 end
 
