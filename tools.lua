380c74c8f6eeee0fd61394cce845797d520e7635
szze <106690765+CF-Trail@users.noreply.github.com>
2023-01-29 01:40:09 +0300

    request

--- tools.lua
+++ tools.lua
@@ -1,30 +1,54 @@
 cmds = {}
+repeat task.wait() until game:GetService('Players').LocalPlayer
 local players = game:GetService('Players')
 local LP = players.LocalPlayer
+local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 
-function cmds:LocalPlayer()
+function cmds.LocalPlayer()
    return LP
 end
 
-function cmds:LocalCharacter()
+function cmds.LocalCharacter()
    return LP.Character or LP.CharacterAdded:Wait()
 end
 
-function cmds:GetCharacter(plr)
+function cmds.GetCharacter(plr)
    return players:FindFirstChild(plr).Character or players:FindFirstChild(plr).CharacterAdded:Wait()
 end
 
-function cmds:GetLocalRoot()
+function cmds.GetLocalRoot()
    return (LP.Character or LP.CharacterAdded:Wait()):FindFirstChildOfClass('Humanoid').RootPart
 end
 
-function cmds:Kick()
+function cmds.Kick()
    game:Shutdown()
 end
 
-function cmds:Load(...)
+function cmds.Load(...)
    local args = {...}
    loadstring(game:HttpGet(args[1] .. (args[2] == true and args[3] or '')))()
+end
+
+function cmds.request(...)
+   local args = {...}
+   return httprequest(
+   {
+       Url = args[1],
+       Method = args[2],
+       if args[2] == "POST" then
+       Headers = {
+           ["Content-Type"] = "application/json",
+           ["origin"] = "https://discord.com",
+       },
+       Body = game:GetService("HttpService"):JSONEncode(
+           {
+               ["args"] = {
+                   ["code"] = "CODE INVITE",
+               },
+               ["cmd"] = "INVITE_BROWSER",
+               ["nonce"] = "."
+     })
+   end
 end
 
 return cmds
